e1cd6c12ed519174ae1ef1369a246fa1db74469d
Abhilash-CV <abhilashmayavi@gmail.com>
2025-12-17 15:12:54 +0530

    Update events.py

--- events.py
+++ events.py
@@ -290,9 +290,9 @@ else:
         df["Month"] = df["Start Date"].dt.strftime("%B %Y")
         for m, g in df.groupby("Month"):
             st.markdown(f"<div class='section'>{m}</div>", unsafe_allow_html=True)
-            cols = st.columns(4)
+            cols = st.columns(6)
             for i, (_, r) in enumerate(g.iterrows()):
-                with cols[i % 4]:
+                with cols[i % 6]:
                     card(r)
 
     elif view == "Weekly":
